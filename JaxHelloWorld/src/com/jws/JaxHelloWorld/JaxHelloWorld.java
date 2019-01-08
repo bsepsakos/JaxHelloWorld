766cfd59a8f43f9d5b551cd512358a7cf5e3350e
SepsakosV <SepsakosV@pc11253.unisystems.gr>
2019-01-08 13:41:44 +0200

    Fibonacci method added

--- JaxHelloWorld/src/com/jws/JaxHelloWorld/JaxHelloWorld.java
+++ JaxHelloWorld/src/com/jws/JaxHelloWorld/JaxHelloWorld.java
@@ -12,5 +12,21 @@ public class JaxHelloWorld {
 	public String Hello(String s) {
 		return "Hello "+ s;
 	}
+	
+	public Integer Fibonacci(int n) throws NegativeNumberException {
+		
+		/* Throw Exception if invalid Arguments */
+		if (n<0) {
+			throw new NegativeNumberException("Negative Numbers are Invalid");
+		}
+		
+		
+		if ((n==1) || (n==0) ) {
+			return 1;
+		}
+		
+		/* Recursive Call */
+		return Fibonacci(n-1) + Fibonacci(n-2) ;
+	}
 
 }
